07f821025db6b510ff51a2e7cc9f065b9c8dff2c
张胜 <1050095803@qq.com>
2018-12-23 21:31:18 +0800

    解决使用对象扩张符会报错问题

--- src/main2.js
+++ src/main2.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Button} from 'antd'
+import {Provider} from 'react-redux'
+import thunk from 'redux-thunk'
+import {createStore,applyMiddleware} from 'redux'
+import reducer from './reducer'
+//import {Button} from 'antd'
 import Counter from './pages/counter.js'
-// const Counter=()=>{
-//     return(
-//         <Button type="primary">按钮</Button>
-//     )
-// }
+//  const Counter=(a)=>{
+//     let b={...a,p:3}
+//      return(
+//          <Button type="primary">{b.p}</Button>
+//      )
+//  }
+ const middleware=[thunk]
+ console.log('thunk>>>>',thunk)
+ console.log('middleware>>>>',middleware)
+ const store=createStore(reducer,applyMiddleware(...middleware))
+ console.log(store.getState());
 
 
-ReactDOM.render(<Counter /> , document.getElementById("study"))
+ReactDOM.render(
+    <Provider store={store}>
+        <Counter />
+    </Provider> , 
+    document.getElementById("study")
+)
 //document.getElementById("study").innerHTML="hello webpack"
